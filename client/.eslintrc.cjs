ceeff8afccb39a69da499971ccabe5a967e7424c
Danilo Leão <olin_ad@hotmail.com>
2023-09-15 10:10:45 -0300

    chore: changed .git path

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            },
+            "rules": {
+              'space-before-function-paren': ['error', 'never']
+            },
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
