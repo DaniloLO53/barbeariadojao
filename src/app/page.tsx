89102c6bdaa80660259573c5434980fad933fcb4
Danilo Leão <olin_ad@hotmail.com>
2023-09-27 09:06:37 -0300

    chore: temporary frontend environment to test deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,42 @@
+'use client'
+
+import axios from "axios";
+import { useEffect, useState } from "react"
+
 export default function Home() {
+  const [dbClicks, setDbClicks] = useState(0);
+  const [clicks, setClicks] = useState(0);
+
+  async function loadClicks() {
+    const { data } = await axios({
+      url: (process.env.NEXT_PUBLIC_SERVER_URL as string) + '/clicks',
+      method: 'get'
+    });
+    setDbClicks(data.clicks);
+  }
+
+  async function updateClicks() {
+    await axios({
+      url: (process.env.NEXT_PUBLIC_SERVER_URL as string) + '/clicks',
+      method: 'put'
+    });
+    setClicks((prevState) => prevState + 1);
+  }
+
+  useEffect(() => {
+    loadClicks();
+  }, []);
+
   return (
     <main>
-      hey, next!
+      <button
+        type="button"
+        onClick={() => updateClicks()}
+      >
+        Click
+      </button>
+      <h1>Clicks: { clicks }</h1>
+      <h1>Db clicks: { dbClicks }</h1>
     </main>
   )
 }
